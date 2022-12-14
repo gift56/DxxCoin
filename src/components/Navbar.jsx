50880e5c8151c101d08aac85dd736efd61b14cd1
gift56 <efeasiughu@gmail.com>
2022-12-14 05:31:18 -0800

    adding logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav>
+      <div className="container">
+        <h1>DXXcoin</h1>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
