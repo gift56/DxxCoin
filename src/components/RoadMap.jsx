ba948b2ad7bb334a8383ff6e0529d7709b18f3cf
gift56 <efeasiughu@gmail.com>
2022-12-14 15:41:59 -0800

    working on road map

--- src/components/RoadMap.jsx
+++ src/components/RoadMap.jsx
@@ -16,8 +16,8 @@ const RoadMap = () => {
           </div>
           <div className="flex flex-col w-full">
             <div className="w-full flex md:flex-row flex-col justify-between items-start">
-              <div className="flex-1">
-                <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 w-[90%]">
+              <div className="flex-1 relative after:content-[''] after:absolute after:top-[-12px] after:right-[8%] after:w-[100px] after:h-[115%] after:border-t-[3px] after:border-b-[3px] after:border-r-[3px] after:rounded-lg after:border-[#17F948] -z-10">
+                <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 w-[90%] relative  z-10 bg-[#021f08]">
                   <h1 className="text-xl font-medium">January</h1>
                   <div className="flex flex-col gap-2 items-start justify-start w-full">
                     <h2 className="text-2xl font-semibold text-white">Brief</h2>
