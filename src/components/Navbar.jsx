6129422ce21818b15e4da870482d1b895ddaf496
gift56 <efeasiughu@gmail.com>
2022-12-14 05:34:26 -0800

    done with logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,9 @@ const Navbar = () => {
     <nav>
       <div className="container">
         <div className="flex items-center justify-between py-5">
-          <h1>DXXcoin</h1>
+          <h1 className="uppercase text-2xl text-white font-bold">
+            DXX<span className="text-[#00FF38]">coin</span>
+          </h1>
         </div>
       </div>
     </nav>
