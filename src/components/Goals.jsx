85dd12f1189d0205584216449340b04214916d4c
gift56 <efeasiughu@gmail.com>
2022-12-14 07:49:12 -0800

    working on some stuff

--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import AboutImg from "../assets/goalImg.svg";
 
 const Goals = () => {
-  return <section id="about"></section>;
+  return (
+    <section id="about">
+      <div className="container">
+        
+      </div>
+    </section>
+  );
 };
 
 export default Goals;
