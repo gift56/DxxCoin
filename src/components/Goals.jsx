ea6469b70807997214091e595e046cf67884e1af
gift56 <efeasiughu@gmail.com>
2022-12-14 08:04:15 -0800

    working on some stuff

--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -6,11 +6,11 @@ const Goals = () => {
     <section id="about">
       <div className="container">
         <div className="flex flex-col w-full gap-4 py-5">
-          <div className="flex gap-4 flex-col w-full items-center justify-center">
+          <div className="flex gap-4 flex-col w-full items-center justify-center px-4">
             <h2 className="text-white text-xl md:text-5xl font-bold">
               Our goal and mission
             </h2>
-            <p>
+            <p className="text-center">
               Whether you’re an experienced trader or just getting started,
               dxxcoin has all the tools you need. Execute your trading
               strategies with the professional level crypto trading platform.
