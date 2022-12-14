fc5b0bc9071f4ce2ab15df5d2a05a7b98354bb64
gift56 <efeasiughu@gmail.com>
2022-12-14 13:34:24 -0800

    added some stuff

--- src/components/Advantage.jsx
+++ src/components/Advantage.jsx
@@ -5,7 +5,9 @@ const Advantage = () => {
     <section id="how">
       <div className="container">
         <div className="flex items-center justify-center flex-col w-full gap-6 py-5">
-            
+          <div className="flex flex-col gap-4">
+            <h1>Our directions and advantages</h1>
+          </div>
         </div>
       </div>
     </section>
