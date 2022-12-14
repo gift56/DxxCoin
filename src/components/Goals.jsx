e209eaebca1d3299a144b64ccf20177773c169a3
gift56 <efeasiughu@gmail.com>
2022-12-14 08:01:50 -0800

    working on some stuff

--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -5,8 +5,10 @@ const Goals = () => {
   return (
     <section id="about">
       <div className="container">
-        <div className="flex flex-col w-full gap-4">
-            
+        <div className="flex flex-col w-full gap-4 py-5">
+          <div className="flex gap-4 flex-col w-full items-center justify-center">
+            <h2>Our goal and mission</h2>
+          </div>
         </div>
       </div>
     </section>
