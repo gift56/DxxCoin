a3975bd29aa638f8541f8e80afb3f7502278a793
gift56 <efeasiughu@gmail.com>
2022-12-14 07:47:06 -0800

    added some updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Goals from "./components/Goals";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Goals />
     </>
   );
 };
