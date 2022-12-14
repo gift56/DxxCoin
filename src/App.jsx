13c1bbecc5c29bdc3495a0e55167b134b0aa27f3
gift56 <efeasiughu@gmail.com>
2022-12-14 05:24:44 -0800

    started working

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Navbar />
+    </>
+  );
+};
 
-export default App
+export default App;
