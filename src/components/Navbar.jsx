46b78f7a982c8a9717c19e25cf0352a5d2e3fdcd
gift56 <efeasiughu@gmail.com>
2022-12-14 05:35:07 -0800

    working on links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
+    
   return (
     <nav>
       <div className="container">
@@ -8,6 +9,9 @@ const Navbar = () => {
           <h1 className="uppercase text-2xl text-white font-bold">
             DXX<span className="text-[#00FF38]">coin</span>
           </h1>
+          <ul className="flex items-center justify-center gap-2">
+
+          </ul>
         </div>
       </div>
     </nav>
