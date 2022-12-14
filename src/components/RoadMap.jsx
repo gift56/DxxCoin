d678c98122ab151cccccd58342bac88a1f4086cd
gift56 <efeasiughu@gmail.com>
2022-12-14 14:39:36 -0800

    working on road map

--- src/components/RoadMap.jsx
+++ src/components/RoadMap.jsx
@@ -5,7 +5,12 @@ const RoadMap = () => {
     <section id="crypto">
       <div className="container">
         <div className="flex items-center justify-center flex-col w-full gap-10 py-5">
-            
+          <div className="flex flex-col gap-4">
+            <h1 className="text-white text-2xl md:text-3xl font-bold text-center">
+              Road map 2022
+            </h1>
+            <p className="text-center md:text-xl text-lg md:px-5 font-normal md:max-w-[75%] max-w-[100%] mx-auto"></p>
+          </div>
         </div>
       </div>
     </section>
