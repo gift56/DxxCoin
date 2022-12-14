0f0271f8aeaf34305cb48ff7bebb56a4e2fdbbd5
gift56 <efeasiughu@gmail.com>
2022-12-14 13:32:22 -0800

    working on goal

--- src/components/Advantage.jsx
+++ src/components/Advantage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Advantage = () => {
-  return <section id="how">Advantage</section>;
+  return (
+    <section id="how">
+      <div className="container">
+        <div className="flex items-center justify-center flex-col w-full gap-6"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Advantage;
