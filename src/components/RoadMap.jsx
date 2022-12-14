2fe15eef0ff215c337cad64c39860e0fd04af316
gift56 <efeasiughu@gmail.com>
2022-12-14 15:50:54 -0800

    working on road map

--- src/components/RoadMap.jsx
+++ src/components/RoadMap.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const RoadMap = () => {
   return (
-    <section id="crypto">
+    <section id="crypto" className="bg-pattern bg-no-repeat">
       <div className="container">
         <div className="flex items-center justify-center flex-col w-full gap-10 py-10">
           <div className="flex flex-col gap-4">
@@ -14,7 +14,7 @@ const RoadMap = () => {
               crypto-collectibles
             </p>
           </div>
-          <div className="flex flex-col w-full">
+          <div className="flex flex-col w-full gap-10">
             <div className="w-full flex md:flex-row flex-col justify-between items-start">
               <div className="flex-1 relative after:content-[''] after:absolute after:top-[-12px] after:right-[8%] after:w-[100px] after:h-[115%] after:border-t-[3px] after:border-b-[3px] after:border-r-[3px] after:rounded-lg after:border-[#17F948] -z-10">
                 <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 w-[90%] relative  z-10 bg-[#021f08]">
@@ -29,7 +29,35 @@ const RoadMap = () => {
                   </div>
                 </div>
               </div>
-              <div className="flex-1">
+              <div className="flex-1 md:mt-[90px]">
+                <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 w-[90%] ml-auto">
+                  <h1 className="text-xl font-medium">January</h1>
+                  <div className="flex flex-col gap-2 items-start justify-start w-full">
+                    <h2 className="text-2xl font-semibold text-white">Brief</h2>
+                    <p className="text-base font-medium">
+                      Lorem ipsum dolor sit amet consectetur. Elit massa erat
+                      vitae non semper quis. Morbi sed aliquet donec facilisis.
+                      Senectus eget.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full flex md:flex-row flex-col justify-between items-start">
+              <div className="flex-1 relative after:content-[''] after:absolute after:top-[-12px] after:right-[8%] after:w-[100px] after:h-[115%] after:border-t-[3px] after:border-b-[3px] after:border-r-[3px] after:rounded-lg after:border-[#17F948] -z-10 mt-[10px]">
+                <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 w-[90%] relative  z-10 bg-[#021f08]">
+                  <h1 className="text-xl font-medium">January</h1>
+                  <div className="flex flex-col gap-2 items-start justify-start w-full">
+                    <h2 className="text-2xl font-semibold text-white">Brief</h2>
+                    <p className="text-base font-medium">
+                      Lorem ipsum dolor sit amet consectetur. Elit massa erat
+                      vitae non semper quis. Morbi sed aliquet donec facilisis.
+                      Senectus eget.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex-1 md:mt-[90px]">
                 <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 w-[90%] ml-auto">
                   <h1 className="text-xl font-medium">January</h1>
                   <div className="flex flex-col gap-2 items-start justify-start w-full">
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,9 @@ module.exports = {
         boxColor: "rgba(255, 255, 255, 0.05)",
         btnColor: "rgba(255, 255, 255, 0.08)",
       },
+      backgroundImage: {
+        pattern: "url(./assets/bgline.svg)",
+      },
     },
   },
   plugins: [],
