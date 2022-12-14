1577b2d1fe1db3d30ac5f8b25a6f6d9b5da49e2c
gift56 <efeasiughu@gmail.com>
2022-12-14 13:30:59 -0800

    working on goal

--- src/components/Advantage.jsx
+++ src/components/Advantage.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Advantage = () => {
-  return (
-    <div>Advantage</div>
-  )
-}
+  return <section id="how">Advantage</section>;
+};
 
-export default Advantage
+export default Advantage;
