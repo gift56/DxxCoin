7690edd4b23be557bb979038ee4d882f685ddb0f
gift56 <efeasiughu@gmail.com>
2022-12-14 14:36:54 -0800

    working on road map

--- src/components/RoadMap.jsx
+++ src/components/RoadMap.jsx
@@ -4,7 +4,9 @@ const RoadMap = () => {
   return (
     <section id="crypto">
       <div className="container">
-        
+        <div className="flex items-center justify-center flex-col w-full gap-10 py-5">
+            
+        </div>
       </div>
     </section>
   );
