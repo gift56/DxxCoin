256a7a6e31d32171af91152256747fc4a8c0f4ca
gift56 <efeasiughu@gmail.com>
2022-12-14 06:36:49 -0800

    working on heroImg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,15 +6,13 @@ const Hero = () => {
   return (
     <section id="home">
       <div className="container">
-        <div className="flex justify-between items-start w-full">
+        <div className="flex justify-between items-start w-full gap-3">
           <div className="flex-1">
             <div className="w-[529px] h-[530px]">
               <img src={HeroImg} alt="hero image" className="w-full h-full" />
             </div>
           </div>
-          <div className="flex-1">
-            
-          </div>
+          <div className="flex-1"></div>
         </div>
       </div>
     </section>
