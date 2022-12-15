ebd60321ce659e58d21ab5bcca2ddc3bf0e9bbff
gift56 <efeasiughu@gmail.com>
2022-12-15 13:29:39 -0800

    working on animation

--- src/components/Advantage.jsx
+++ src/components/Advantage.jsx
@@ -32,9 +32,14 @@ const Advantage = () => {
       <div className="container">
         <div className="flex items-center justify-center flex-col w-full gap-10 py-5">
           <div className="flex flex-col gap-4">
-            <h1 className="text-white text-2xl md:text-5xl font-bold text-center">
+            <motion.h1
+              initial={{ opacity: 0, y: "-20px" }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ type: "spring", duration: 2 }}
+              className="text-white text-2xl md:text-5xl font-bold text-center"
+            >
               Our directions and advantages
-            </h1>
+            </motion.h1>
             <p className="text-center md:text-xl text-lg md:px-5 font-normal md:max-w-[75%] max-w-[100%] mx-auto">
               Integrate once and never worry about scaling again. We ensure
               composability between ecosystem projects by maintaining a single
@@ -43,7 +48,10 @@ const Advantage = () => {
           </div>
           <div className="w-full flex md:flex-row flex-col justify-center items-center gap-4">
             {walletCard.map(({ img, title, details }, i) => (
-              <div key={i} className="md:w-[35%] w-full border border-[#ffffff19] rounded-[24px] p-3 flex items-center flex-col gap-2">
+              <div
+                key={i}
+                className="md:w-[35%] w-full border border-[#ffffff19] rounded-[24px] p-3 flex items-center flex-col gap-2"
+              >
                 <div className="w-[166px] h-[115px]">
                   <img src={img} alt={title} className="w-full h-full" />
                 </div>
