4e6b1418ca136725aabdbfd5a456b9e1f3ede733
gift56 <efeasiughu@gmail.com>
2022-12-15 13:38:33 -0800

    working on animation

--- src/components/RoadMap.jsx
+++ src/components/RoadMap.jsx
@@ -30,7 +30,7 @@ const RoadMap = () => {
             </motion.p>
           </div>
           <div className="flex flex-col w-full md:gap-10 gap-5">
-            <div className="w-full flex md:flex-row flex-col justify-between items-start gap-4 md:gap-0">
+            <div className="w-full flex md:flex-row flex-col justify-between items-start gap-4 md:gap-0 overflow-hidden">
               <div className="flex-1 relative md:after:content-[''] md:after:absolute md:after:top-[-12px] md:after:right-[8%] md:after:w-[100px] md:after:h-[115%] md:after:border-t-[3px] md:after:border-b-[3px] md:after:border-r-[3px] md:after:rounded-lg md:after:border-[#17F948] -z-10">
                 <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 md:w-[90%] relative  z-10 bg-[#021f08] w-full">
                   <h1 className="text-xl font-medium">January</h1>
