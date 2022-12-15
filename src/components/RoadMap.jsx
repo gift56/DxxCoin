999efde6ca9b8fa041b9b04cd827f5989be6ca63
gift56 <efeasiughu@gmail.com>
2022-12-14 16:04:43 -0800

    done positioning some stuff

--- src/components/RoadMap.jsx
+++ src/components/RoadMap.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const RoadMap = () => {
   return (
-    <section id="crypto" className="bg-pattern bg-no-repeat bg-bgPose bg-[length:90%_80%]">
+    <section
+      id="crypto"
+      className="md:bg-pattern md:bg-no-repeat md:bg-bgPose md:bg-[length:90%_80%]"
+    >
       <div className="container">
         <div className="flex items-center justify-center flex-col w-full gap-16 py-10 ">
           <div className="flex flex-col gap-4">
@@ -14,10 +17,10 @@ const RoadMap = () => {
               crypto-collectibles
             </p>
           </div>
-          <div className="flex flex-col w-full gap-10">
-            <div className="w-full flex md:flex-row flex-col justify-between items-start">
-              <div className="flex-1 relative after:content-[''] after:absolute after:top-[-12px] after:right-[8%] after:w-[100px] after:h-[115%] after:border-t-[3px] after:border-b-[3px] after:border-r-[3px] after:rounded-lg after:border-[#17F948] -z-10">
-                <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 w-[90%] relative  z-10 bg-[#021f08]">
+          <div className="flex flex-col w-full md:gap-10 gap-5">
+            <div className="w-full flex md:flex-row flex-col justify-between items-start gap-4 md:gap-0">
+              <div className="flex-1 relative md:after:content-[''] md:after:absolute md:after:top-[-12px] md:after:right-[8%] md:after:w-[100px] md:after:h-[115%] md:after:border-t-[3px] md:after:border-b-[3px] md:after:border-r-[3px] md:after:rounded-lg md:after:border-[#17F948] -z-10">
+                <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 md:w-[90%] relative  z-10 bg-[#021f08] w-full">
                   <h1 className="text-xl font-medium">January</h1>
                   <div className="flex flex-col gap-2 items-start justify-start w-full">
                     <h2 className="text-2xl font-semibold text-white">Brief</h2>
@@ -30,7 +33,7 @@ const RoadMap = () => {
                 </div>
               </div>
               <div className="flex-1 md:mt-[90px]">
-                <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 w-[90%] ml-auto">
+                <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 md:w-[90%] relative  z-10 bg-[#021f08] w-full">
                   <h1 className="text-xl font-medium">January</h1>
                   <div className="flex flex-col gap-2 items-start justify-start w-full">
                     <h2 className="text-2xl font-semibold text-white">Brief</h2>
@@ -43,9 +46,9 @@ const RoadMap = () => {
                 </div>
               </div>
             </div>
-            <div className="w-full flex md:flex-row flex-col justify-between items-start">
-              <div className="flex-1 relative after:content-[''] after:absolute after:top-[-12px] after:right-[8%] after:w-[100px] after:h-[115%] after:border-t-[3px] after:border-b-[3px] after:border-r-[3px] after:rounded-lg after:border-[#17F948] -z-10 mt-[10px]">
-                <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 w-[90%] relative  z-10 bg-[#021f08]">
+            <div className="w-full flex md:flex-row flex-col justify-between items-start gap-5 md:gap-0">
+              <div className="flex-1 relative md:after:content-[''] md:after:absolute md:after:top-[-12px] md:after:right-[8%] md:after:w-[100px] md:after:h-[115%] md:after:border-t-[3px] md:after:border-b-[3px] md:after:border-r-[3px] md:after:rounded-lg md:after:border-[#17F948] -z-10">
+                <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 md:w-[90%] relative  z-10 bg-[#021f08] w-full">
                   <h1 className="text-xl font-medium">January</h1>
                   <div className="flex flex-col gap-2 items-start justify-start w-full">
                     <h2 className="text-2xl font-semibold text-white">Brief</h2>
@@ -58,7 +61,7 @@ const RoadMap = () => {
                 </div>
               </div>
               <div className="flex-1 md:mt-[90px]">
-                <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 w-[90%] ml-auto">
+                <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 md:w-[90%] relative  z-10 bg-[#021f08] w-full">
                   <h1 className="text-xl font-medium">January</h1>
                   <div className="flex flex-col gap-2 items-start justify-start w-full">
                     <h2 className="text-2xl font-semibold text-white">Brief</h2>
