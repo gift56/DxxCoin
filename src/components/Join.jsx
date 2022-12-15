6da9a1df8e56647a39cb297642f5b7de8bf810b6
gift56 <efeasiughu@gmail.com>
2022-12-15 03:30:32 -0800

    added some stuff

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { BsTwitter, BsInstagram } from "react-icons/bs";
 import { FaTelegram, FaFacebook } from "react-icons/fa";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { MdOutlineMailOutline } from "react-icons/md";
 
 const Join = () => {
   return (
     <section id="join">
       <div className="container">
         <div className="flex items-center md:flex-row justify-center flex-col w-full gap-5 py-10">
-            
+          <div className="flex-1 flex flex-col gap-4"></div>
         </div>
       </div>
     </section>
