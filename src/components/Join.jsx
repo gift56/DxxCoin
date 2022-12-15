13746fee540591d15638da2d3dfd1b00b3c112f1
gift56 <efeasiughu@gmail.com>
2022-12-15 03:24:31 -0800

    working on join

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BsTwitter, BsInstagram } from "react-icons/bs";
-import { FaTelegram } from "react-icons/fa";
+import { FaTelegram, FaFacebook } from "react-icons/fa";
 
 const Join = () => {
   return (
