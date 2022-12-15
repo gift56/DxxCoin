40f319f037e61c12922c940b98ecc72af010edfb
gift56 <efeasiughu@gmail.com>
2022-12-15 03:41:05 -0800

    added some stuff

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -5,11 +5,36 @@ import { HiOutlineLocationMarker } from "react-icons/hi";
 import { MdOutlineMailOutline } from "react-icons/md";
 
 const Join = () => {
+  const socialIcons = [
+    {
+      icon: <BsInstagram className="w-[25px] h-[25px] text-[#ffffffb7]" />,
+      to: "instagram.com",
+    },
+    {
+      icon: <BsTwitter className="w-[25px] h-[25px] text-[#ffffffb7]" />,
+      to: "twitter.com",
+    },
+    {
+      icon: <FaTelegram className="w-[25px] h-[25px] text-[#ffffffb7]" />,
+      to: "telegram.com",
+    },
+    {
+      icon: <FaFacebook className="w-[25px] h-[25px] text-[#ffffffb7]" />,
+      to: "facebook.com",
+    },
+  ];
   return (
     <section id="join">
       <div className="container">
-        <div className="flex items-center md:flex-row justify-center flex-col w-full gap-5 py-10">
-          <div className="flex-1 flex flex-col gap-4"></div>
+        <div className="flex items-start md:flex-row justify-between flex-col w-full gap-5 py-10">
+          <div className="flex-1 flex flex-col gap-4">
+            <div className="flex md:justify-start items-center justify-center gap-4">
+              {socialIcons.map(({ icon, to }) => (
+                <a key={to}>{icon}</a>
+              ))}
+            </div>
+          </div>
+          <div className="flex-1"></div>
         </div>
       </div>
     </section>
