cf3419f360911a203650bcc1ef53a749aeed2cba
gift56 <efeasiughu@gmail.com>
2022-12-15 03:21:06 -0800

    working on join

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Join = () => {
   return (
     <section id="join">
-      <div className="container"></div>
+      <div className="container">
+        <div className="flex items-center md:flex-row justify-center flex-col w-full gap-5 py-10 "></div>
+      </div>
     </section>
   );
 };
