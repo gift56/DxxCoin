0f4d977d4306058ae59b977b9ba7f8a0f3c81e95
gift56 <efeasiughu@gmail.com>
2022-12-15 03:19:30 -0800

    working on join

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Join = () => {
-  return <section id="join">Join</section>;
+  return (
+    <section id="join">
+      <div className="container"></div>
+    </section>
+  );
 };
 
 export default Join;
