6605a1efb53aeff98d67dab151b092b0a92499ea
gift56 <efeasiughu@gmail.com>
2022-12-15 03:46:27 -0800

    working on location

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -23,16 +23,30 @@ const Join = () => {
       to: "facebook.com",
     },
   ];
+
+  const contactInfo = [
+    {
+      icon: <HiOutlineLocationMarker />,
+      locate: "Turks and caicos Island, over back",
+    },
+  ];
+
   return (
     <section id="join">
       <div className="container">
         <div className="flex items-start md:flex-row justify-between flex-col w-full gap-5 py-10">
-          <div className="flex-1 flex flex-col gap-4">
-            <div className="flex md:justify-start items-center justify-center gap-4">
-              {socialIcons.map(({ icon, to }) => (
-                <a key={to}>{icon}</a>
-              ))}
+          <div className="flex-1 flex flex-col gap-9">
+            <div className="flex flex-col gap-8">
+              <div className="flex md:justify-start items-center justify-center gap-4">
+                {socialIcons.map(({ icon, to }) => (
+                  <a key={to}>{icon}</a>
+                ))}
+              </div>
+              <h2 className="md:text-xl text-lg text-white font-normal">
+                “If you can dream it, you can do it”
+              </h2>
             </div>
+            <div className="flex flex-col gap-6 items-start justify-start"></div>
           </div>
           <div className="flex-1"></div>
         </div>
