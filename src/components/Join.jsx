f6d2afdb80f1324dfd80ced3805072541d785fdb
gift56 <efeasiughu@gmail.com>
2022-12-15 03:24:57 -0800

    working on join

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -6,7 +6,9 @@ const Join = () => {
   return (
     <section id="join">
       <div className="container">
-        <div className="flex items-center md:flex-row justify-center flex-col w-full gap-5 py-10 "></div>
+        <div className="flex items-center md:flex-row justify-center flex-col w-full gap-5 py-10">
+            
+        </div>
       </div>
     </section>
   );
