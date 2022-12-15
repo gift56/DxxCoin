d3a619613f925163459cc2becf654783dc5dd9e9
gift56 <efeasiughu@gmail.com>
2022-12-15 13:42:21 -0800

    working on animation

--- src/components/RoadMap.jsx
+++ src/components/RoadMap.jsx
@@ -8,8 +8,8 @@ const RoadMap = () => {
       className="md:bg-pattern md:bg-no-repeat md:bg-bgPose md:bg-[length:90%_80%] relative"
     >
       <div className="hidden md:flex bg-[#55DF77] w-[370px] h-[200px] rounded-[50%] blur-[190px] absolute z-[-2] right-0"></div>
-      <div className="container">
-        <div className="flex items-center justify-center flex-col w-full gap-16 py-10 ">
+      <div className="container overflow-hidden">
+        <div className="flex items-center justify-center flex-col w-full gap-16 py-10">
           <div className="flex flex-col gap-4">
             <motion.h1
               initial={{ opacity: 0, y: "50px" }}
@@ -30,9 +30,14 @@ const RoadMap = () => {
             </motion.p>
           </div>
           <div className="flex flex-col w-full md:gap-10 gap-5">
-            <div className="w-full flex md:flex-row flex-col justify-between items-start gap-4 md:gap-0 overflow-hidden">
+            <div className="w-full flex md:flex-row flex-col justify-between items-start gap-4 md:gap-0">
               <div className="flex-1 relative md:after:content-[''] md:after:absolute md:after:top-[-12px] md:after:right-[8%] md:after:w-[100px] md:after:h-[115%] md:after:border-t-[3px] md:after:border-b-[3px] md:after:border-r-[3px] md:after:rounded-lg md:after:border-[#17F948] -z-10">
-                <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 md:w-[90%] relative  z-10 bg-[#021f08] w-full">
+                <motion.div
+                  initial={{ opacity: 0, x: "-50px" }}
+                  whileInView={{ opacity: 1, x: 0 }}
+                  transition={{ type: "spring", duration: 2 }}
+                  className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 md:w-[90%] relative  z-10 bg-[#021f08] w-full"
+                >
                   <h1 className="text-xl font-medium">January</h1>
                   <div className="flex flex-col gap-2 items-start justify-start w-full">
                     <h2 className="text-2xl font-semibold text-white">Brief</h2>
@@ -42,10 +47,15 @@ const RoadMap = () => {
                       Senectus eget.
                     </p>
                   </div>
-                </div>
+                </motion.div>
               </div>
               <div className="flex-1 md:mt-[90px] relative md:after:content-[''] md:after:absolute md:after:top-[-12px] md:after:left-[8%] md:after:w-[100px] md:after:h-[115%] md:after:border-t-[3px] md:after:border-b-[3px] md:after:border-l-[3px] md:after:rounded-lg md:after:border-[#17F948] -z-10">
-                <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 md:w-[90%] relative  z-10 bg-[#021f08] w-full ml-auto">
+                <motion.div
+                  initial={{ opacity: 0, x: "50px" }}
+                  whileInView={{ opacity: 1, x: 0 }}
+                  transition={{ type: "spring", duration: 3 }}
+                  className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 md:w-[90%] relative  z-10 bg-[#021f08] w-full ml-auto"
+                >
                   <h1 className="text-xl font-medium">Fabruary</h1>
                   <div className="flex flex-col gap-2 items-start justify-start w-full">
                     <h2 className="text-2xl font-semibold text-white">
@@ -57,12 +67,17 @@ const RoadMap = () => {
                       Senectus eget.
                     </p>
                   </div>
-                </div>
+                </motion.div>
               </div>
             </div>
             <div className="w-full flex md:flex-row flex-col justify-between items-start gap-5 md:gap-0">
               <div className="flex-1 relative md:after:content-[''] md:after:absolute md:after:top-[-12px] md:after:right-[8%] md:after:w-[100px] md:after:h-[115%] md:after:border-t-[3px] md:after:border-b-[3px] md:after:border-r-[3px] md:after:rounded-lg md:after:border-[#17F948] -z-10">
-                <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 md:w-[90%] relative  z-10 bg-[#021f08] w-full">
+                <motion.div
+                  initial={{ opacity: 0, x: "-50px" }}
+                  whileInView={{ opacity: 1, x: 0 }}
+                  transition={{ type: "spring", duration: 3 }}
+                  className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 md:w-[90%] relative  z-10 bg-[#021f08] w-full"
+                >
                   <h1 className="text-xl font-medium">March</h1>
                   <div className="flex flex-col gap-2 items-start justify-start w-full">
                     <h2 className="text-2xl font-semibold text-white">
@@ -74,10 +89,15 @@ const RoadMap = () => {
                       Senectus eget.
                     </p>
                   </div>
-                </div>
+                </motion.div>
               </div>
               <div className="flex-1 md:mt-[90px] relative md:after:content-[''] md:after:absolute md:after:top-[-12px] md:after:left-[8%] md:after:w-[100px] md:after:h-[115%] md:after:border-t-[3px] md:after:border-b-[3px] md:after:border-l-[3px] md:after:rounded-lg md:after:border-[#17F948] -z-10">
-                <div className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 md:w-[90%] relative  z-10 bg-[#021f08] w-full ml-auto">
+                <motion.div
+                  initial={{ opacity: 0, x: "50px" }}
+                  whileInView={{ opacity: 1, x: 0 }}
+                  transition={{ type: "spring", duration: 2 }}
+                  className="border-[#00ff3775] rounded-lg p-4 pb-6 flex flex-col items-center justify-center border gap-4 md:w-[90%] relative  z-10 bg-[#021f08] w-full ml-auto"
+                >
                   <h1 className="text-xl font-medium">April</h1>
                   <div className="flex flex-col gap-2 items-start justify-start w-full">
                     <h2 className="text-2xl font-semibold text-white">
@@ -89,7 +109,7 @@ const RoadMap = () => {
                       Senectus eget.
                     </p>
                   </div>
-                </div>
+                </motion.div>
               </div>
             </div>
           </div>
